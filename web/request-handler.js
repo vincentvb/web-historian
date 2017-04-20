2902a2339e54b4685826134d280d015838062667
Hack Reactor Student <student@hackreactor.com>
2017-04-19 20:34:41 -0700

    Completed all tests

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,49 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs')
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+exports.handleRequest = function (request, response) {
+	if (request.method === "POST") {
+		var newData = ""
+		request.on('data', (data) => {
+
+			newData += data
+		})
+		request.on('end', () => {
+			newData = newData.slice(4)
+			archive.addUrlToList(newData + "\n", function() {
+				response.writeHead(302)
+				response.end("Stored!")
+			})
+		})
+	}
+  if (request.method === "GET") {
+			if (request.url === "/") {
+				response.writeHead(200)
+				var htmlFile = path.join(__dirname, "/public/index.html")
+				fs.readFile(htmlFile, (error, data) => {
+					response.write(data);
+					response.end();
+
+				})
+
+			}
+  		archive.isUrlArchived(request.url.slice(1), function(isTrue) {
+				 if (!isTrue) {
+				 	response.writeHead(404);
+					response.end("TRY AGAIN");
+				}
+				else if (isTrue) {
+					fs.readFile(path.join(archive.paths.archivedSites + request.url), function(error, data) {
+						if (error) throw error;
+						response.write(data);
+						response.end();
+					})
+				}
+			})
+
+
+	}
+  // res.end(archive.paths.list);
 };
